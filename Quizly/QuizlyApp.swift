50f506db4fa916d4aca1cc128207bdfa4cd74979
Cem Bıçakcı <cmbicakci@gmail.com>
2025-08-02 22:23:26 +0300

    first commit

---
+++ Quizly/QuizlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizlyApp.swift
+//  Quizly
+//
+//  Created by Cem Bıçakcı on 2.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct QuizlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
